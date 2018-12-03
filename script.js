dec84378d068dc06a1f3b493ab29f6140c613c59
Moritztz <tm.283wings@gmail.com>
2018-12-03 13:21:16 +0900

    e

--- script.js
+++ script.js
@@ -172,6 +172,18 @@ $('#user6').click(function () {
     $('#callto-id').val("tc6");
 });
 
+$('#ln1').click(function () {
+    setCallOption(true, MAIN_VIDEO_CODEC);
+    getpeerid("ln1");
+    $('#callto-id').val("ALR1");
+});
+
+$('#ln2').click(function () {
+    setCallOption(true, MAIN_VIDEO_CODEC);
+    getpeerid("ln2");
+    $('#callto-id').val("ALR2");
+});
+
 $('#videot').click(function () {
     setCallOption(false, 'H264');
     getpeerid("videoT");
